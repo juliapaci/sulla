ebda5873271cc6ecd75436319731d2688b841e06
aymey <fabianpaci@gmail.com>
2024-02-26 19:46:35 +1100

    default layout

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ impl egui_dock::TabViewer for TabViewer {
     }
 
     fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
-        match tab.to_string() {
+        match tab.as_str() {
+            "File" => self.file_tab(ui),
             _ => {
                 ui.label(format!("Empty {tab} contents"));
             }
@@ -20,8 +21,17 @@ impl egui_dock::TabViewer for TabViewer {
     }
 }
 
+impl TabViewer {
+    fn file_tab(&self, ui: &mut egui::Ui) {
+        // ui.
+    }
+}
+
 struct SullaApp {
-    tree: DockState<String>
+    tree: DockState<String>,
+    dropped_files: Vec<egui::DroppedFile>,
+    picked_path: Option<String>
+
 }
 
 impl Default for SullaApp {
@@ -30,7 +40,7 @@ impl Default for SullaApp {
 
         let [timeline, assets] =
             tree.main_surface_mut()
-                .split_left(NodeIndex::root(), 0.35, vec!["Assets".to_owned(), "Files".to_owned()]);
+                .split_left(NodeIndex::root(), 0.35, vec!["Assets".to_owned(), "File".to_owned()]);
         let [_, _inspector] =
             tree.main_surface_mut()
                 .split_below(assets, 0.5, vec!["Inspector".to_owned()]);
@@ -38,7 +48,11 @@ impl Default for SullaApp {
             tree.main_surface_mut()
                 .split_above(timeline, 0.5, vec!["Player".to_owned(), "Scene".to_owned()]);
 
-        Self { tree }
+        Self {
+            tree,
+            dropped_files: vec![],
+            picked_path: None
+        }
     }
 }
 
@@ -47,6 +61,62 @@ impl eframe::App for SullaApp {
         DockArea::new(&mut self.tree)
             .style(Style::from_egui(ctx.style().as_ref()))
             .show(ctx, &mut TabViewer {});
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Drag-and-drop files onto the window!");
+
+            if ui.button("Open file…").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.picked_path = Some(path.display().to_string());
+                }
+            }
+
+            if let Some(picked_path) = &self.picked_path {
+                ui.horizontal(|ui| {
+                    ui.label("Picked file:");
+                    ui.monospace(picked_path);
+                });
+            }
+
+            // Show dropped files (if any):
+            if !self.dropped_files.is_empty() {
+                ui.group(|ui| {
+                    ui.label("Dropped files:");
+
+                    for file in &self.dropped_files {
+                        let mut info = if let Some(path) = &file.path {
+                            path.display().to_string()
+                        } else if !file.name.is_empty() {
+                            file.name.clone()
+                        } else {
+                            "???".to_owned()
+                        };
+
+                        let mut additional_info = vec![];
+                        if !file.mime.is_empty() {
+                            additional_info.push(format!("type: {}", file.mime));
+                        }
+                        if let Some(bytes) = &file.bytes {
+                            additional_info.push(format!("{} bytes", bytes.len()));
+                        }
+                        if !additional_info.is_empty() {
+                            info += &format!(" ({})", additional_info.join(", "));
+                        }
+
+                        ui.label(info);
+                    }
+                });
+            }
+        });
+
+        preview_files_being_dropped(ctx);
+
+        // Collect dropped files:
+        ctx.input(|i| {
+            if !i.raw.dropped_files.is_empty() {
+                self.dropped_files = i.raw.dropped_files.clone();
+            }
+        });
     }
 }
 
