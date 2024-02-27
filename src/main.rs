fa9ff67fab0c5cc9d6c5960cffe3c4c2dae182b8
aymey <fabianpaci@gmail.com>
2024-02-27 20:55:31 +1100

    button

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use egui::{Frame, Ui};
 
 use egui_dock::{DockArea, DockState, NodeIndex, Style};
 
@@ -22,16 +23,15 @@ impl egui_dock::TabViewer for TabViewer {
 }
 
 impl TabViewer {
-    fn file_tab(&self, ui: &mut egui::Ui) {
-        // ui.
+    fn file_tab(&mut self, ui: &mut egui::Ui) {
+        if ui.button("open file").clicked() {
+
+        }
     }
 }
 
 struct SullaApp {
     tree: DockState<String>,
-    dropped_files: Vec<egui::DroppedFile>,
-    picked_path: Option<String>
-
 }
 
 impl Default for SullaApp {
@@ -40,7 +40,7 @@ impl Default for SullaApp {
 
         let [timeline, assets] =
             tree.main_surface_mut()
-                .split_left(NodeIndex::root(), 0.35, vec!["Assets".to_owned(), "File".to_owned()]);
+                .split_left(NodeIndex::root(), 0.25, vec!["Assets".to_owned(), "File".to_owned()]);
         let [_, _inspector] =
             tree.main_surface_mut()
                 .split_below(assets, 0.5, vec!["Inspector".to_owned()]);
@@ -49,74 +49,29 @@ impl Default for SullaApp {
                 .split_above(timeline, 0.5, vec!["Player".to_owned(), "Scene".to_owned()]);
 
         Self {
-            tree,
-            dropped_files: vec![],
-            picked_path: None
+            tree
         }
     }
 }
 
 impl eframe::App for SullaApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        DockArea::new(&mut self.tree)
-            .style(Style::from_egui(ctx.style().as_ref()))
-            .show(ctx, &mut TabViewer {});
+        egui::CentralPanel::default()
+            .frame(Frame::central_panel(&ctx.style()).inner_margin(0.))
+            .show(ctx, |ui| {
+                DockArea::new(&mut self.tree)
+                    .style(Style::from_egui(ctx.style().as_ref()))
+                    .show(ctx, &mut TabViewer {});
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Drag-and-drop files onto the window!");
-
-            if ui.button("Open file…").clicked() {
-                if let Some(path) = rfd::FileDialog::new().pick_file() {
-                    self.picked_path = Some(path.display().to_string());
+                if ui
+                    .selectable_label(match self.tree.find_active_focused() {
+                        Some(focused) => focused.1 == "File",
+                        None => false
+                    }, "open file")
+                    .clicked() {
+                    println!("test");
                 }
-            }
-
-            if let Some(picked_path) = &self.picked_path {
-                ui.horizontal(|ui| {
-                    ui.label("Picked file:");
-                    ui.monospace(picked_path);
-                });
-            }
-
-            // Show dropped files (if any):
-            if !self.dropped_files.is_empty() {
-                ui.group(|ui| {
-                    ui.label("Dropped files:");
-
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
-                        } else {
-                            "???".to_owned()
-                        };
-
-                        let mut additional_info = vec![];
-                        if !file.mime.is_empty() {
-                            additional_info.push(format!("type: {}", file.mime));
-                        }
-                        if let Some(bytes) = &file.bytes {
-                            additional_info.push(format!("{} bytes", bytes.len()));
-                        }
-                        if !additional_info.is_empty() {
-                            info += &format!(" ({})", additional_info.join(", "));
-                        }
-
-                        ui.label(info);
-                    }
-                });
-            }
-        });
-
-        preview_files_being_dropped(ctx);
-
-        // Collect dropped files:
-        ctx.input(|i| {
-            if !i.raw.dropped_files.is_empty() {
-                self.dropped_files = i.raw.dropped_files.clone();
-            }
-        });
+            });
     }
 }
 
